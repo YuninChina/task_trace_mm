31dab122c0495108cb6595eaa71478fd99aa46ca
leon <372588334@qq.com>
2021-10-14 07:31:05 -0700

    modified:   init/main.c

--- init/main.c
+++ init/main.c
@@ -12,14 +12,33 @@
 int main(void)
 {
 	void *p1,*p2,*p3;
+	int cnt = 0;
 	p1 = MALLOC(10);
 	assert(p1);
 	p2 = MALLOC(20);
 	assert(p1);
 	p3 = MALLOC(30);
 	assert(p1);
-	
-	task_mm_show();
+
+	while(1)
+	{
+		system("clear");
+		task_mm_show();
+		sleep(1);
+		if(cnt == 3*1)
+		{
+			if(p1) {FREE(p1); p1=NULL;}
+		}
+		if(cnt == 3*2)
+		{
+			if(p2) {FREE(p2); p2=NULL;}
+		}
+		if(cnt == 3*3)
+		{
+			if(p3) {FREE(p3); p3=NULL;}
+		}
+		cnt++;
+	}
 	
 	return 0;
 }
